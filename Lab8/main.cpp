ac4d76a8ff70f82cd6282957b5a2d16b5d1449fa
Arthur Lee Jones <arthrjns@outlook.com>
2019-11-21 19:04:22 -0600

    Update main.cpp

--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -1,3 +1,11 @@
+// --------------------------------------------------------------------------------
+// AUTHOR: Arthur Lee Jones
+// FILENAME: main.cpp
+// SPECIFICATION: Max Heap
+// FOR: CS 2413 Data Structure Section 503
+// --------------------------------------------------------------------------------
+
+
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
@@ -5,26 +13,21 @@
 using namespace std;
 
 
-
-typedef struct NodeB {
-	char data;
-	NodeB *right,*left;
-} Node; //A struct node that holds the values - float and a pointer to the next node.
-
-
 int getRight(int index);
 int getLeft(int index);
 void Max_heapify(int index);
 int isFull(int number);
-void createTree();
+void printTree();
 void buildHeap();
 int getUserSelection(int lowerNum, int greaterNum);
 int getUserNumberChoice();
 void callFunctions(int userInput, int* running);
 void printMenu();
+void deleteRootNode();
+int isEmpty();
 
-int heapStack[101];
-int heapStackMarker = 0;
+int heapStack[101]; //the whole heap
+int heapStackMarker = 0; //the heap marker
 int main(int argc, char **argv)
 {
 	cout << "Welcome to Lab 8! This program will Max heapify your input." << endl << endl;
@@ -38,36 +41,35 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-
+// NAME: Max_heapify
+// INPUT PARAMETERS: index, a number of the last node's index .
+// OUTPUT: NONE
+// PURPOSE: The function will order the user given heap into a max heap.
+//---------------
 void Max_heapify(int index){
-//	cout << index << " index at the top." << endl;
 	if(index == -1) return;
-	int forChildren = index;
-	int leftSibling = getLeft(forChildren);
-	int rightSibling = getRight(forChildren);
-	int largest = 0;
-	int tempHolder = 0;
-	int flag = 0;
-	cout << heapStack[index] << " parent. "<< heapStack[leftSibling] << " left. " << heapStack[rightSibling] << " right." << endl;
+	int leftSibling = getLeft(index); //left child
+	int rightSibling = getRight(index); //right child
+	int largest = 0; //the largest number's index
+	int flag = 0; //a flag that will tell us if we found a new larger element.
+	
 	if( (leftSibling <= heapStackMarker) && (heapStack[leftSibling] > heapStack[index]) ) {
 		largest = leftSibling;
 	} else {
 		largest = index;
 	}
+	
 	if( (rightSibling <= heapStackMarker) && (heapStack[rightSibling] > heapStack[largest]) ) {
 		largest = rightSibling;
 	}
-	cout << index << " the current. " << endl;
-	cout << leftSibling << " the left." << endl;
-	cout << rightSibling << " the right." << endl;
-	cout << largest << " the largest." << endl;
-	cout << "------------" << endl;
+	
 	if(largest != index) {
-		tempHolder = heapStack[index];
+		int tempHolder = heapStack[index]; //the temp holder for swaping out the values.
 		heapStack[index] = heapStack[largest];
 		heapStack[largest] = tempHolder;
 		flag = 1;
 	}
+	
 	if(flag == 1) {
 		Max_heapify(largest);
 	} else {
@@ -77,40 +79,58 @@ void Max_heapify(int index){
 	return;
 }
 
+// NAME: getRight
+// INPUT PARAMETERS: index, a number of the current node's index.
+// OUTPUT: NONE
+// PURPOSE: The function will return the index number of the right child.
+//---------------
 int getRight(int index){
 	index = 2 * index + 2;
 	return index;
 }
 
+// NAME: getLeft
+// INPUT PARAMETERS: index, a number of the current node's index.
+// OUTPUT: NONE
+// PURPOSE: The function will return the index number of the left child.
+//---------------
 int getLeft(int index){
-	index = 2* index +1;
+	index = 2* index + 1;
 	return index;
 }
 
-int getParent(int index) {
-	index = index / 2;
-	return index;
-}
-void createTree() {
-	cout << heapStack[0] << endl;
-	cout << heapStack[1] << endl;
-	cout << heapStack[2] << endl;
-	cout << heapStack[3] << endl;
-	cout << heapStack[4] << endl;
-	cout << heapStack[5] << endl;
-	cout << heapStack[6] << endl;
-	cout << heapStack[7] << endl;
-	cout << heapStack[8] << endl;
-	cout << heapStack[9] << endl;
-	cout << heapStack[10] << endl;
-	cout << heapStack[11] << endl;
-	cout << heapStack[12] << endl;
-	cout << heapStack[13] << endl;
-	cout << heapStack[14] << endl;
+
+// NAME: printTree
+// INPUT PARAMETERS: NONE
+// OUTPUT: NONE
+// PURPOSE: The function will print the heap in a tree order.
+//---------------
+void printTree() {
+	if(isEmpty()) {
+		cout << "Your heap is empty please enter values and try again." << endl;
+		return;
+	}
+	int x = 0; // A count that will tell us when to endl after so many elements
+	for(int i = 0; i < heapStackMarker; i++) {
+		cout << heapStack[i];
+		if(i == x){
+			cout << endl;
+			x = 2*x+2;
+		} else {
+			cout << " ";
+		}
+	}
+	cout << endl;
 }
 
+
+// NAME: buildHeap
+// INPUT PARAMETERS: NONE
+// OUTPUT: NONE
+// Side Effect: Will build a heap and ++heapStackMarker
+// PURPOSE: The function will take user input and build a heap accordingly
+//---------------
 void buildHeap(){
-	int userInput = 0;
 	cout << "Enter the values for the heap. Type any negative number to exit." << endl;
 	for(;;) {
 		if(isFull(heapStackMarker)) {
@@ -118,21 +138,39 @@ void buildHeap(){
 			break;
 		}
 		cout << "Please enter a number: ";
-		userInput = getUserNumberChoice();
+		int userInput = getUserNumberChoice(); //user input
 		if(userInput < 0) break;
 		heapStack[heapStackMarker] = userInput;
 		
 		heapStackMarker++;
 	}
 	Max_heapify(heapStackMarker-1);
-	createTree();
 }
 
 
+// NAME: isFull
+// INPUT PARAMETERS: NONE
+// OUTPUT: NONE
+// Side Effect: Will set the running pointer to 0
+// PURPOSE: The function will check if the heap is full
+//---------------
 int isFull(int number) {
 	if(number == 50) return 1;
 	return 0;
 }
+
+
+// NAME: isEmpty
+// INPUT PARAMETERS: NONE
+// OUTPUT: NONE
+// PURPOSE: The function will check if the heap is empty
+//---------------
+int isEmpty() {
+	if(heapStackMarker == 0) return 1;
+	return 0;
+} 
+
+
 // NAME: printMenu
 // INPUT PARAMETERS: None
 // OUTPUT: None
@@ -149,16 +187,16 @@ void printMenu() {
 
 // NAME: getUserNumberChoice
 // INPUT PARAMETERS: NONE
-// OUTPUT: NONE
+// OUTPUT: userInput, a number the user wants
 // Side Effect: Will get the user choice for the number for the node
-// PURPOSE: The function will error check and return the choice of the user for the number for the node
+// PURPOSE: The function will error check and return the choice of the user for the number
 //---------------
 int getUserNumberChoice() {
 	int userInput = 0; //The user input
 	int flag = 0; //a flag if true it will continue to run the function
 	cin >> userInput;
 	if( cin.fail() ) {
-		cout << "Please enter a number: ";
+		cout << "Please enter a proper number: ";
 		cin.clear();
 		cin.ignore(256, '\n');
 		flag = 1;
@@ -167,11 +205,11 @@ int getUserNumberChoice() {
 	return userInput;
 }
 
-// NAME: getUserNumberChoice
-// INPUT PARAMETERS: NONE
-// OUTPUT: NONE
-// Side Effect: Will get the user choice for the number for the node
-// PURPOSE: The function will error check and return the choice of the user for the number for the node
+// NAME: getUserSelection
+// INPUT PARAMETERS: lowerNum, a lower number for a selection; greaterNum, a greater number for a selection
+// OUTPUT: userInput, a number the user wants.
+// Side Effect: Will get the user choice for the menu
+// PURPOSE: The function will error check and return the choice of the user for the menu
 //---------------
 int getUserSelection(int lowerNum, int greaterNum) {
 	int userInput = 0; //The user input
@@ -198,9 +236,27 @@ void exitProgram(int *running){
 	return;
 }
 
+
+// NAME: deleteRootNode
+// INPUT PARAMETERS: NONE
+// OUTPUT: NONE
+// Side Effect: Will subtract one from heapStackMarker I.E deleting a node
+// PURPOSE: The function will replace the last node with the top node then delete the last node. 
+//---------------
+void deleteRootNode() {
+	if(isEmpty()) {
+		cout << "Your heap is empty please enter values and try again." << endl;
+		return;
+	}
+	heapStack[0] = heapStack[heapStackMarker-1];
+	heapStackMarker--;
+	Max_heapify(heapStackMarker-1);
+}
+
+
 // NAME: callFunctions
-// INPUT PARAMETERS: userInput, The input given by the user; Tree, the whole Tree; running, a pointer to running to stop the program
-// OUTPUT: It will return a Tree
+// INPUT PARAMETERS: userInput, The input given by the user; running, a pointer to running to stop the program
+// OUTPUT: NONE
 // PURPOSE: The function will take the user input and call the function that the user requested.
 //----------------
 void callFunctions(int userInput, int* running) {
@@ -209,8 +265,10 @@ void callFunctions(int userInput, int* running) {
 			buildHeap();
 			break;
 		case 2:
+			deleteRootNode();
 			break;
 		case 3:
+			printTree();
 			break;
 		case 4:
 			exitProgram(running);
